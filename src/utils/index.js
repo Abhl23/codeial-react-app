e1b557539f411677a100c564d8e9bc05a74f1ea9
Abhi <bhatt23abhishek@gmail.com>
2022-08-12 20:37:57 +0530

    create getFormBody function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,14 @@
 export * from './constants';
+
+export const getFormBody = (params) => {
+    let formBody=[];
+
+    for(let property in params){
+        let encodedKey=encodeURIComponent(property);    // 'user name' => 'user%20name'
+        let encodedValue=encodeURIComponent(params[property]);     //'abhi 123' => 'abhi%20123'
+
+        formBody.push(encodedKey + '=' + encodedValue);
+    }
+
+    return formBody.join('&');      //returns the array as a string seperated by '&'
+};
