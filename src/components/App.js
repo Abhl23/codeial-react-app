eb48c665a57236dfe3eff007a1d9b044cc5ebb2d
Abhi <bhatt23abhishek@gmail.com>
2022-08-15 14:46:54 +0530

    create PrivateRoute component for Settings page

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,47 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import { Home, Login, Signup, Settings } from '../pages';
-import {Loader, Navbar} from './';
+import { Loader, Navbar } from './';
 import { useAuth } from '../hooks';
 
-
 const Page404 = () => {
-  return (
-    <h1>404 : Page not found</h1>
-  );
+  return <h1>404 : Page not found</h1>;
+};
+
+const PrivateRoute = ({ children }) => {
+  const auth = useAuth();
+
+  if (auth.user) {
+    return children;
+  }
+
+  return <Navigate replace to="/login" />;
 };
 
 function App() {
+  const auth = useAuth();
 
-  const auth=useAuth();
-
-  if(auth.loading){
+  if (auth.loading) {
     return <Loader />;
   }
 
   return (
     <div className="App">
       <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="*" element={<Page404 />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/settings"
+          element={
+            <PrivateRoute>
+              <Settings />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
     </div>
   );
 }
